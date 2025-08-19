4b4fea7bf32617cea143ab561edd89dd60460224
sss2611 <solss2611@gmail.com>
2025-08-18 22:16:25 -0300

    le faltaria el enlace de wpp y el carrito

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,22 @@
+// // Seleccionamos todas las imágenes con clase 'modal-trigger'
+// const modalImages = document.querySelectorAll('.modal-trigger');
+// const modalImage = document.getElementById('modalImage');
+
+// modalImages.forEach(img => {
+//   img.addEventListener('click', () => {
+//     modalImage.src = img.src; // Cambia la imagen del modal
+//     const myModal = new bootstrap.Modal(document.getElementById('productModal'));
+//     myModal.show();
+//     modalImage.classList.remove('zoomed'); // Quitamos zoom si estaba activo
+//   });
+// });
+
+// // Zoom al hacer click en la imagen del modal
+// modalImage.addEventListener('click', () => {
+//   modalImage.classList.toggle('zoomed');
+// });
+
+
 // Seleccionamos todas las imágenes con clase 'modal-trigger'
 const modalImages = document.querySelectorAll('.modal-trigger');
 const modalImage = document.getElementById('modalImage');
